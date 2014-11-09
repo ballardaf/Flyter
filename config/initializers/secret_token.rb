a0fd8329c6e6905ea13c822f0e656e09a8cfd038
ballardaf <ballardaf@gmail.com>
2014-11-08 22:37:46 -0600

    Flyter Startup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flyter::Application.config.secret_key_base = '0d87446d3ca119445bb070644b40ff43639f1207d4033185c1c4657573e2f6ef1a03199af9142faf2b61ed56e9fd6473fbfdd299ccbca4d993398d7117e71ab6'
